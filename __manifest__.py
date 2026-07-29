cc7a68338371998db116b18ce262b5c0bf01d6f2
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-29 17:22:28 -0500

    FIX: Habilitar overflow-y: auto en contenedor principal para permitir scroll vertical (v18.0.1.0.17)

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Panamá - Reportes Diarios Cierre de Caja (MBA Consultings)",
-    "version": "18.0.1.0.16",
+    "version": "18.0.1.0.17",
     "category": "Accounting/Localizations",
     "summary": "Reportes diarios agnósticos: Cierre de caja para facturación, cobros CxC, POS y matriz mensual MTD. | MBA Consultings",
     "author": "MBA Consultings, Brooks Gonzalez",
